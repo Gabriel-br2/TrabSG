a2a346d0357d8e12018ce9bf70276e4ba220759a
FernandoPM28 <ferpivamendes@gmail.com>
2024-08-07 14:10:38 -0300

    presentation changes

--- Trab_Test.c
+++ Trab_Test.c
@@ -6,14 +6,14 @@
 
 void display(void) {
     glClearColor (1.0, 1.0, 1.0, 0.0);
-    glClear (GL_COLOR_BUFFER_BIT);
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
     glShadeModel (GL_SMOOTH);
 
-    float light_position[4] = {1, 1, 1, 0};
+    float light_position[4] = {0, 0, 7, 0};
     float light_ambient[4] = {0.2, 0.2, 0.2, 1.0};
     float light_diffuse[4] = {0.7, 0.7, 0.7, 0.7};
     float light_specular[4] = {1.0, 1.0, 1.0, 1.0};
@@ -24,8 +24,8 @@ void display(void) {
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 
     float material_ambient[4] = {0.1, 0.1, 0.1, 1.0};
-    float material_diffuse[4] = {0.2, 0.2, 0.2, 1.0};
-    float material_specular[4]= {0.2, 0.2, 0.1, 1.0};
+    float material_diffuse[4] = {0.5, 0.5, 0.5, 1.0};
+    float material_specular[4]= {0.4, 0.4, 0.4, 1.0};
     float material_shininess[1] = {10.0};
     
     glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
@@ -35,10 +35,9 @@ void display(void) {
 
     glLoadIdentity ();
 
-
-
-    glTranslatef(0, 0, -10);
-    glRotatef(-35,1,0,0);
+    glTranslatef(0, -1, -12);
+    glRotatef(-12,0,1,0);
+    glRotatef(-45,1,0,0);
 
     glPushMatrix();
     glScalef(2, 5, 1);
@@ -52,18 +51,32 @@ void display(void) {
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(3, -2.7, 0);
+    glTranslatef(3, -2.6, 0);
     glRotatef(-25,0,0,1);
-    glScalef(3, 2, 1);
+    glScalef(3.1, 2, 1);
     glutSolidCube(1);
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(-3, -2.7, 0);
+    glTranslatef(-3, -2.6, 0);
     glRotatef(25,0,0,1);
-    glScalef(3, 2, 1);
+    glScalef(3.1, 2, 1);
     glutSolidCube(1);
     glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-3, -2.6, 0);
+    glRotatef(25,0,0,1);
+    glScalef(3.9, 1.7, 1);
+    glutSolidCube(1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(3, -2.6, 0);
+    glRotatef(-25,0,0,1);
+    glScalef(3.9, 1.7, 1);
+    glutSolidCube(1);
+    glPopMatrix();   
 
     glPushMatrix();
     glTranslatef(1, 3, 0);
@@ -79,7 +92,21 @@ void display(void) {
     glutSolidCube(1);
     glPopMatrix();
 
+    float ground_ambient[4] = {0.1, 0.1, 0.1, 1.0};
+    float ground_diffuse[4] = {0.1, 0.9, 0.1, 1.0};
+    float ground_specular[4]= {0.1, 0.9, 0.1, 1.0};
+    float ground_shininess[1] = {10.0};
+    
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ground_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, ground_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, ground_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, ground_shininess);
 
+    glPushMatrix();
+    glTranslatef(0, 2, -1);
+    glScalef(15, 16, 1);
+    glutSolidCube(1);
+    glPopMatrix();
 
     glFlush ();
 }
@@ -88,15 +115,15 @@ void reshape (int w, int h) {
     glViewport (0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode (GL_PROJECTION);
     glLoadIdentity ();
-    glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+    glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 25.0);
     //glOrtho (-6.0, 6.0, -6.0, 6.0, 1.5, 20.0);
     glMatrixMode (GL_MODELVIEW);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (600, 600);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (800, 800);
     glutInitWindowPosition (10, 10);
     glutCreateWindow ("C3 building 3d display");
     glutDisplayFunc(display);
